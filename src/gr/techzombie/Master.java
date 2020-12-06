2155a86f20f8713d5ad15e82a64223d6f4708820
kosmas1991 <kosmasgou@gmail.com>
2020-12-06 20:54:48 +0200

    add functional menu LOL

---
+++ src/gr/techzombie/Master.java
@@ -0,0 +1,32 @@
+package gr.techzombie;
+
+import java.util.ArrayList;
+
+public class Master {
+    private final String name;
+    private final ArrayList<Bank> banks;
+
+    public ArrayList<String> getBanksNames() {
+        ArrayList<String> onomata = new ArrayList<>();
+        for (Bank i : banks)
+            onomata.add(i.getName());
+        return onomata;
+    }
+
+    public Master(String name) {
+        this.name = name;
+        this.banks = new ArrayList<>();
+    }
+
+    public void addNewBank(String name){
+        banks.add(new Bank(name));
+
+    }
+
+    public Bank searchBank(String name){
+        for (Bank i : banks)
+            if (i.getName().equals(name))
+                return i;
+        return null;
+    }
+}
